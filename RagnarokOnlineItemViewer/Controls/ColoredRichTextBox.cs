a679393ab973b9d09ceca04231e6c405430813b1
Armin Herling <armin.herling@gmail.com>
2018-08-24 22:49:20 +0200

    Added a custom rich text box for binding the text

---
+++ RagnarokOnlineItemViewer/Controls/ColoredRichTextBox.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Media;
+
+namespace RagnarokOnlineItemViewer.Controls
+{
+    public class ColoredRichTextBox : RichTextBox
+    {
+        public static readonly DependencyProperty RichTextProperty = DependencyProperty.Register(
+            "Text",
+            typeof( string ),
+            typeof( ColoredRichTextBox ),
+            new PropertyMetadata( null, ColoredRichTextPropertyChanged ) );
+
+        public string Text
+        {
+            get => (string)GetValue( RichTextProperty );
+            set => SetValue( RichTextProperty, value );
+        }
+
+        private static void ColoredRichTextPropertyChanged( DependencyObject dependencyObject, DependencyPropertyChangedEventArgs dependencyPropertyChangedEventArgs )
+        {
+            ColoredRichTextBox richTextBox = (ColoredRichTextBox)dependencyObject;
+            richTextBox.Document.Blocks.Clear();
+
+            var paragraph = new Paragraph();
+            richTextBox.Document.Blocks.Add( paragraph );
+
+            var text = (string)dependencyPropertyChangedEventArgs.NewValue;
+            paragraph.Inlines.Add( new Run( text ) );
+        }
+    }
+}
