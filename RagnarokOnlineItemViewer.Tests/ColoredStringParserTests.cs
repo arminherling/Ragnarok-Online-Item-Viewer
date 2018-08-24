a1b3fdf4da278ba438f83835e840202e7b29852a
Armin Herling <armin.herling@gmail.com>
2018-08-25 00:20:02 +0200

    Added colored string parser

---
+++ RagnarokOnlineItemViewer.Tests/ColoredStringParserTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace RagnarokOnlineItemViewer.Tests
+{
+    [TestFixture]
+    public class ColoredStringParserTests
+    {
+        [Test]
+        public void Parse_ReturnsEmptyList_OnEmptyString()
+        {
+            var expected = new List<(string, Brush)>();
+
+            var actual = ColoredStringParser.Parse( "" );
+
+            Assert.AreEqual( expected, actual );
+        }
+    }
+}
---
+++ RagnarokOnlineItemViewer/ColoredStringParser.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Windows.Media;
+
+namespace RagnarokOnlineItemViewer
+{
+    public class ColoredStringParser
+    {
+        public static List<(string, Brush)> Parse( string v )
+        {
+            return new List<(string, Brush)>();
+        }
+    }
+}
