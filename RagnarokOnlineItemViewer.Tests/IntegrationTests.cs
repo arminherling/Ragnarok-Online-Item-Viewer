51f5c81a78ccb798c8548e143002a458172801f4
Armin Herling <armin.herling@gmail.com>
2018-08-24 18:39:31 +0200

    Added more integration tests

--- RagnarokOnlineItemViewer.Tests/IntegrationTests.cs
+++ RagnarokOnlineItemViewer.Tests/IntegrationTests.cs
@@ -11,17 +11,46 @@ namespace RagnarokOnlineItemViewer.Tests
         [Test]
         public void CurrentDetailsView_ReturnsDetailsOfSelectedItem_AfterSettingNewSelectedItem()
         {
-            var expectedItem = new Item( 
-                id: "501", 
-                name: "Red Potion", 
+            var expectedItem = new Item(
+                id: "501",
+                name: "Red Potion",
                 description: "A potion made from ground Red Herbs that restores about 45 HP." );
-            var itemsViewModel = new ItemsViewModel(new FakeItemRepository());
+            var itemsViewModel = new ItemsViewModel( new FakeItemRepository() );
 
             itemsViewModel.SelectedItem = expectedItem;
-           
+
             Assert.AreEqual( expectedItem.ID, itemsViewModel.CurrentDetailsViewModel.ID );
             Assert.AreEqual( expectedItem.Name, itemsViewModel.CurrentDetailsViewModel.Name );
             Assert.AreEqual( expectedItem.Description, itemsViewModel.CurrentDetailsViewModel.Description );
+        }
+
+        [Test]
+        public void CurrentDetailsView_ReturnsDetailsOfFirstItem_IfThereAreAnyItems()
+        {
+            var expectedItem = new Item(
+                id: "501",
+                name: "Red Potion",
+                description: "A potion made from ground Red Herbs that restores about 45 HP." );
+            var fakeRepo = new FakeItemRepository();
+            fakeRepo.AddFake( expectedItem );
+
+            var itemsViewModel = new ItemsViewModel( fakeRepo );
+
+            Assert.AreEqual( expectedItem.ID, itemsViewModel.CurrentDetailsViewModel.ID );
+            Assert.AreEqual( expectedItem.Name, itemsViewModel.CurrentDetailsViewModel.Name );
+            Assert.AreEqual( expectedItem.Description, itemsViewModel.CurrentDetailsViewModel.Description );
+        }
+
+        [Test]
+        public void CurrentDetailsView_ReturnsEmptyStrings_IfThereAreNoItems()
+        {
+            var fakeRepo = new FakeItemRepository();
+
+            var itemsViewModel = new ItemsViewModel( fakeRepo );
+
+            Assert.IsEmpty( itemsViewModel.CurrentDetailsViewModel.ID );
+            Assert.IsEmpty( itemsViewModel.CurrentDetailsViewModel.Name );
+            Assert.IsEmpty( itemsViewModel.CurrentDetailsViewModel.Description );
         }
     }
 }
