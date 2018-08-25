104966dd601c1026abfc57a202e88cb246668339
Armin Herling <armin.herling@gmail.com>
2018-08-26 01:46:46 +0200

    Fleshed out the colored string parser

--- RagnarokOnlineItemViewer.Tests/ColoredStringParserTests.cs
+++ RagnarokOnlineItemViewer.Tests/ColoredStringParserTests.cs
@@ -31,8 +31,61 @@ namespace RagnarokOnlineItemViewer.Tests
             var result = ColoredStringParser.Parse( input );
 
             Assert.AreEqual( expectedCount, result.Count );
-            Assert.AreEqual( result[0].Item1, input );
-            Assert.AreEqual( result[0].Item2, expectedBrush );
+            Assert.AreEqual( input, result[0].Item1 );
+            Assert.AreEqual( expectedBrush, result[0].Item2 );
+        }
+
+        [Test]
+        public void Parse_ReturnsFullStringWithRedBrush_OnStringWithRedHexCodeAtTheStart()
+        {
+            var input = "^ff0000Some text about stuff";
+            var expectedText = "Some text about stuff";
+            var expectedCount = 1;
+            var expectedBrush = (Brush)Brushes.Red;
+
+            var result = ColoredStringParser.Parse( input );
+
+            Assert.AreEqual( expectedCount, result.Count );
+            Assert.AreEqual( expectedText, result[0].Item1 );
+            Assert.AreEqual( expectedBrush.ToString(), result[0].Item2.ToString() );
+        }
+
+        [Test]
+        public void Parse_ReturnsMultipleStringsWithCorrectBrush_OnStringWithMultipleHexCodes()
+        {
+            var input = "^FF0000Some text about stuff ^008000and different color in the second half";
+            var expectedText1 = "Some text about stuff ";
+            var expectedBrush1 = (Brush)Brushes.Red;
+            var expectedText2 = "and different color in the second half";
+            var expectedBrush2 = (Brush)Brushes.Green;
+            var expectedCount = 2;
+
+            var result = ColoredStringParser.Parse( input );
+
+            Assert.AreEqual( expectedCount, result.Count );
+            Assert.AreEqual( expectedText1, result[0].Item1 );
+            Assert.AreEqual( expectedBrush1.ToString(), result[0].Item2.ToString() );
+            Assert.AreEqual( expectedText2, result[1].Item1 );
+            Assert.AreEqual( expectedBrush2.ToString(), result[1].Item2.ToString() );
+        }
+
+        [Test]
+        public void Parse_ReturnsMultipleStringsWithCorrectBrush_OnStringWithRedHexCodeInTheMiddle()
+        {
+            var input = "Some text with a ^FF0000different color in the second half";
+            var expectedText1 = "Some text with a ";
+            var expectedBrush1 = (Brush)Brushes.Black;
+            var expectedText2 = "different color in the second half";
+            var expectedBrush2 = (Brush)Brushes.Red;
+            var expectedCount = 2;
+
+            var result = ColoredStringParser.Parse( input );
+
+            Assert.AreEqual( expectedCount, result.Count );
+            Assert.AreEqual( expectedText1, result[0].Item1 );
+            Assert.AreEqual( expectedBrush1.ToString(), result[0].Item2.ToString() );
+            Assert.AreEqual( expectedText2, result[1].Item1 );
+            Assert.AreEqual( expectedBrush2.ToString(), result[1].Item2.ToString() );
         }
     }
 }
--- RagnarokOnlineItemViewer/ColoredStringParser.cs
+++ RagnarokOnlineItemViewer/ColoredStringParser.cs
@@ -1,16 +1,51 @@
 ﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using System.Windows.Media;
 
 namespace RagnarokOnlineItemViewer
 {
-    public class ColoredStringParser
+    public static class ColoredStringParser
     {
+        private static readonly string _pattern = @"\^[0-9a-f]{6}";
+        private static readonly RegexOptions _options = RegexOptions.IgnoreCase;
+        private static BrushConverter _converter = new BrushConverter();
+
         public static List<(string, Brush)> Parse( string input )
         {
             var result = new List<(string, Brush)>();
 
             if( !string.IsNullOrWhiteSpace( input ) )
-                result.Add( (input, Brushes.Black) );
+            {
+                var matches = Regex.Matches( input, _pattern, _options );
+                var substringStart = -1;
+                var substringEnd = -1;
+
+                foreach( Match m in matches )
+                {
+                    // first match isn't at the start of the input, so we need 
+                    // to add the text before that as result with the default color
+                    if( substringStart == -1 && m.Index != 0 )
+                        result.Add( (input.Substring( 0, m.Index ), Brushes.Black) );
+
+                    substringStart = m.Index + m.Length;
+                    if( m.NextMatch().Success )
+                    {
+                        substringEnd = m.NextMatch().Index;
+                    }
+                    else
+                    {
+                        // last match
+                        substringEnd = input.Length;
+                    }
+
+                    var hex = m.Value.Replace( '^', '#' );
+                    var brush = (Brush)_converter.ConvertFromString( hex );
+                    result.Add( (input.Substring( substringStart, substringEnd - substringStart ), brush) );
+                }
+
+                if( matches.Count == 0 )
+                    result.Add( (input, Brushes.Black) );
+            }
 
             return result;
         }
