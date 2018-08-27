4afde389a4618e1cf614712a844d84457b4c2522
Armin Herling <armin.herling@gmail.com>
2018-08-27 21:29:23 +0200

    Added an id converter that the returns the path to the small image, or a default icon if the id is not found

---
+++ RagnarokOnlineItemViewer/Converters/IdToIconConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Windows.Data;
+
+namespace RagnarokOnlineItemViewer.Converters
+{
+    public class IdToIconConverter : IValueConverter
+    {
+        public object Convert( object value, Type targetType, object parameter, CultureInfo culture )
+        {
+            var directoryPath = Path.GetDirectoryName( Process.GetCurrentProcess().MainModule.FileName );
+            var iconPath = Path.Combine( directoryPath, $"Data\\{value}.gif" );
+            if( File.Exists( iconPath ) )
+                return iconPath;
+
+            return Path.Combine( directoryPath, "Data\\unknown.png" );
+        }
+
+        public object ConvertBack( object value, Type targetType, object parameter, CultureInfo culture )
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
