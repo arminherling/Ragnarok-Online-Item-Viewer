5aedc15646d2797ba19eda19253884a49873a78b
Armin Herling <armin.herling@gmail.com>
2018-08-28 22:31:16 +0200

    Passing another test

--- RagnarokOnlineItemViewer.Tests/IdToIconConverterTests.cs
+++ RagnarokOnlineItemViewer.Tests/IdToIconConverterTests.cs
@@ -17,14 +17,18 @@ namespace RagnarokOnlineItemViewer.Tests
             var actualPath1 = (string)converter.Convert( "501", null, "Data|.gif", null );
             var actualPath2 = (string)converter.Convert( "7897", null, "Data|.gif", null );
 
-            Assert.IsTrue( actualPath1.EndsWith(expected1) );
-            Assert.IsTrue( actualPath2.EndsWith(expected2) );
+            Assert.IsTrue( actualPath1.EndsWith( expected1 ) );
+            Assert.IsTrue( actualPath2.EndsWith( expected2 ) );
         }
 
         [Test]
-        public void IdToIconConverter_ReturnsIconForId_OnIdWithIconImageButInvalidParameter()
+        public void IdToIconConverter_ReturnsNull_OnIdWithIconImageButInvalidParameter()
         {
-            Assert.Fail();
+            var converter = new IdToIconConverter();
+
+            var result = converter.Convert( "501", null, "InvalidFolder|.xyz", null );
+
+            Assert.IsNull( result );
         }
 
         [Test]
