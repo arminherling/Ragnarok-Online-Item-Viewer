e0d586b6e288186bbaaaf03efcf26e7146e84876
Armin Herling <armin.herling@gmail.com>
2018-09-02 22:28:54 +0200

    Added a sort adorner to the item list

--- RagnarokOnlineItemViewer/ItemSorting.cs
+++ RagnarokOnlineItemViewer/ItemSorting.cs
@@ -1,25 +1,38 @@
 ﻿using System.ComponentModel;
 using System.Windows.Controls;
 using System.Windows.Data;
+using System.Windows.Documents;
 
 namespace RagnarokOnlineItemViewer
 {
     public class ItemSorting
     {
+        private GridViewColumnHeader _sortHeader;
+        private SortAdorner _sortAdorner;
         private ListSortDirection _sortDirection = ListSortDirection.Descending;
 
-        public void Sort(object columnHeader, CollectionView list )
+        public void Sort(object sender, CollectionView list )
         {
-            string column = ( columnHeader as GridViewColumnHeader )?.Content?.ToString();
-            if( column == null )
+            var columnHeader = ( sender as GridViewColumnHeader );
+            if( columnHeader == null )
                 return;
+
+            list.SortDescriptions.Clear();
+
+            if(_sortHeader != null )
+                AdornerLayer.GetAdornerLayer( _sortHeader ).Remove( _sortAdorner );
 
             if( _sortDirection == ListSortDirection.Descending )
                 _sortDirection = ListSortDirection.Ascending;
             else
                 _sortDirection = ListSortDirection.Descending;
 
-            list.SortDescriptions.Clear();
+            _sortHeader = columnHeader;
+
+            _sortAdorner = new SortAdorner( _sortHeader, _sortDirection );
+            AdornerLayer.GetAdornerLayer( _sortHeader ).Add( _sortAdorner );
+
+            var column = columnHeader.Content.ToString();
             list.SortDescriptions.Add( new SortDescription( column, _sortDirection ) );
         }
     }
---
+++ RagnarokOnlineItemViewer/SortAdorner.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel;
+using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Media;
+
+namespace RagnarokOnlineItemViewer
+{
+    public class SortAdorner : Adorner
+    {
+        private static Geometry _ascendingArrow = Geometry.Parse( "M 5,5 15,5 10,0 5,5" );
+        private static Geometry _descendingArrow = Geometry.Parse( "M 5,0 10,5 15,0 5,0" );
+        private readonly Geometry _geometry;
+
+        public SortAdorner( UIElement adornedElement, ListSortDirection sortDirection ) 
+            : base( adornedElement )
+        {
+            _geometry = ( sortDirection == ListSortDirection.Ascending ) 
+                ? _ascendingArrow 
+                : _descendingArrow;
+        }
+
+        protected override void OnRender( DrawingContext drawingContext )
+        {
+            var translateTransformation = new TranslateTransform( 
+                AdornedElement.RenderSize.Width - 20, 
+                ( AdornedElement.RenderSize.Height - 5 ) / 2 );
+
+            if(translateTransformation.X >= 20 )
+            {
+                drawingContext.PushTransform( translateTransformation );
+                drawingContext.DrawGeometry( Brushes.Black, null, _geometry );
+                drawingContext.Pop();
+            }
+        }
+    }
+}
